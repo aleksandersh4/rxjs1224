74fb076a84bf8451f556aa7b010ed31397fb8982
ashabanov <ashabanov@t1-consulting.ru>
2024-12-02 00:10:43 +0300

    feat(rxjs): init start rxjs course

---
+++ index.ts
@@ -0,0 +1,10 @@
+/*
+const sequence = new Promise((res) => {
+    let count = 2;
+    setInterval(() => {
+        res(count++);
+    })
+});
+
+sequence.then((v) => console.log(v));
+sequence.then((v) => console.log(v));*/
